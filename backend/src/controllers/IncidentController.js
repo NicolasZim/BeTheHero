348bf901f7f01f570caa026a1353b700e8a8326b
Nicolas Zim <>
2020-03-29 01:24:10 -0300

    Fix'd. Não dava display no numero de whatsapp
    No backend acabei esquecendo de enviar o whatsapp junto.

--- backend/src/controllers/IncidentController.js
+++ backend/src/controllers/IncidentController.js
@@ -6,19 +6,23 @@ module.exports = {
 
     const [count] = await connection('incidents').count();
 
-    console.log(count);
-
+    
     const incidents = await connection('incidents')
-    .join('ongs', 'ongs.id', '=', 'incidents.ong_id')
-    .limit(5)
-    .offset((page - 1) * 5)
-    .select(['incidents.*',
+      .join('ongs', 'ongs.id', '=', 'incidents.ong_id')
+      .limit(5)
+      .offset((page - 1) * 5)
+      .select(['incidents.*',
       'ongs.name',
       'ongs.email',
+      'ongs.whatsapp',
       'ongs.city',
       'ongs.uf'
     ]);
-
+    // Sanity Check bellow
+    // console.log('Incidents', incidents);
+    // console.log('Whatsapp:', incidents[0].whatsapp);
+    // console.log('Email:',incidents.email);
+  
     response.header('X-Total-Count', count['count(*)']);
 
     return response.json(incidents);
